99b02882fef3fc37af4273e75873052645348e2e
Prathakur1508 <109587991+Prathakur1508@users.noreply.github.com>
2022-08-05 12:59:51 +0530

    Add files via upload

---
+++ spring-security-client/src/main/java/com/stream/client/SpringSecurityClientApplication.java
@@ -0,0 +1,13 @@
+package com.stream.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityClientApplication.class, args);
+	}
+
+}
